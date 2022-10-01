958502236d891ccef515eae3fb54ada316333ba5
Ignacio Dotta <ij.dotta@gmail.com>
2022-10-01 16:40:42 -0300

    allow mass assignable attributes

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,6 +13,8 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    protected $guarded = [];
+
     /**
      * The attributes that are mass assignable.
      *
@@ -22,6 +24,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role_id',
     ];
 
     /**
