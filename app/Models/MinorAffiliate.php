d66b91349736ed35fff42ea370831f604a5f5cbf
NicolasLeidi <71295543+NicolasLeidi@users.noreply.github.com>
2022-09-30 21:29:53 -0300

    Probé lo que pude sin hacer ingreso a la base de datos, disculpen.

---
+++ app/Models/MinorAffiliate.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MinorAffiliate extends Model
+{
+    use HasFactory;
+
+    public static function storeMinorAffiliate($name, $surname, $dateBirth, $DNI, $phone, $adultAffiliateID){
+        
+        if(isset($name)){
+            $affiliate=new MinorAffiliate();
+
+            $affiliate->name=$name;
+            $affiliate->sur_name=$surname;
+            $affiliate->date_of_birth=$dateBirth;
+            $affiliate->DNI=$DNI;
+            $affiliate->phone=$phone;
+            $affiliate->adultAffiliateID=$adultAffiliateID;
+
+            $affiliate->save();
+
+        }
+        
+    }
+}
