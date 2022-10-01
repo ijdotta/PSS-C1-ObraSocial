8e4e4797f9427c2637d9026f31ca63fb9cd71b0f
Tomas Felipe <tomasfelipe911@gmail.com>
2022-10-01 20:35:46 -0300

    Se termino de implementar la visualizacion de planes (incluyendo la vista de prestaciones y precios)

--- resources/views/queries/showPlans.blade.php
+++ resources/views/queries/showPlans.blade.php
@@ -133,37 +133,37 @@
 
                                     <!-- Modal precios-->
                                     <div class="modal fade" id="Precios{{$plan->name}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                    <div class="modal-dialog">
-                                        <div class="modal-content">
-                                        <div class="modal-header">
-                                            <h5 class="modal-title" id="exampleModalLabel">Precios</h5>
-                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                        </div>
-                                        <div class="modal-body">
-                                             <div class="row align-items-start">
-                                                <label class="col">Precio menores de 25</label>
-                                                <input type="text" value={{$plan->price_under_25}} class="col" id="floatingInput" name="name" disabled>
+                                        <div class="modal-dialog">
+                                            <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="exampleModalLabel">Precios</h5>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                             </div>
-                                            <div class="row align-items-start">
-                                                <label class="col">Precio de 25 a 40</label>
-                                                <input type="text" value={{$plan->price_from_25_to_40}} class="col" id="floatingInput" name="name" disabled>
-                                            </div>
-                                            <div class="row align-items-start">
-                                                <label class="col">Precio de 40 a 60</label>
-                                                <input type="text" value={{$plan->price_from_40_to_60}} class="col" id="floatingInput" name="name" disabled>
-                                            </div>
-                                            <div class="row align-items-start">
-                                                <label class="col">Precio mayores de 60</label>
-                                                <input type="text" value={{$plan->price_over_60}} class="col" id="floatingInput" name="name" disabled>
-                                            </div>
+                                            <div class="modal-body">
+                                                <div class="row align-items-start">
+                                                    <label class="col">Precio menores de 25</label>
+                                                    <input type="text" value={{$plan->price_under_25}} class="col" id="floatingInput" name="name" disabled>
+                                                </div>
+                                                <div class="row align-items-start">
+                                                    <label class="col">Precio de 25 a 40</label>
+                                                    <input type="text" value={{$plan->price_from_25_to_40}} class="col" id="floatingInput" name="name" disabled>
+                                                </div>
+                                                <div class="row align-items-start">
+                                                    <label class="col">Precio de 40 a 60</label>
+                                                    <input type="text" value={{$plan->price_from_40_to_60}} class="col" id="floatingInput" name="name" disabled>
+                                                </div>
+                                                <div class="row align-items-start">
+                                                    <label class="col">Precio mayores de 60</label>
+                                                    <input type="text" value={{$plan->price_over_60}} class="col" id="floatingInput" name="name" disabled>
+                                                </div>
 
-                                        <div class="modal-footer">
-                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Volver</button>
-                                            <button type="button" class="btn btn-primary">Modificar</button>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Volver</button>
+                                                <button type="button" class="btn btn-primary">Modificar</button>
+                                            </div>
+                                            </div>
                                         </div>
-                                        </div>
-                                    </div>
-                                    </div>
+                                    </div></div>
 
                                     <h1> - </h1>
 
