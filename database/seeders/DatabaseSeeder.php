f9b6ac79457d337b1a7eb02eb14cce851e57136b
Ignacio Dotta <ij.dotta@gmail.com>
2022-10-02 20:34:22 -0300

    fixed seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,11 +4,11 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Enums\EmployeeRole;
+use App\Enums\UserRole;
 use App\Models\Address;
 use App\Models\Employee;
-use App\Models\Plan;
 use App\Models\User;
-use App\Models\Role;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,26 +20,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        Role::create(['name' => 'admin']);
-        Role::create(['name' => 'Jefe de area']);
-        Role::create(['name' => 'Administrativo']);
-        Role::create(['name' => 'Cliente']);
-
-        $adminRoleId = Role::where(['name' => 'admin'])->get()->first()->id;
 
         User::create([
-            'role_id' => $adminRoleId,
+            'role' => UserRole::EMPLOYEE->name,
             'email' => 'admin@os.com',
             'password' => '1234'
         ]);
 
-        User::factory(10)->create(['role_id' => $adminRoleId]);
-
-        Plan::factory(10)->create();
-
         Employee::factory(10)
-                ->state(function () use ($adminRoleId) { return [
-                    'user_id' => function () use ($adminRoleId) { return User::factory(1)->createOne(['role_id' => $adminRoleId])->id; },
+                ->state(function () { return [
+                    'user_id' => function () { return User::factory(1)->createOne(['role' => EmployeeRole::ADMIN->name])->id; },
                     'address_id' => function () { return Address::factory(1)->createOne()->id; },
                     // 'plan_id' => function () { return Plan::factory(1)->createOne()->id; }
                     // 'plan_id' => function () { return Plan::pluck('id')->random(); }
