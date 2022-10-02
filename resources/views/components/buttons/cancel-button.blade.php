7dd2d8ebe11f0432b2b1a539d1b213ae4595d6b3
Ignacio Dotta <ij.dotta@gmail.com>
2022-10-01 21:59:43 -0300

    submit and cancel buttons comps

---
+++ resources/views/components/buttons/cancel-button.blade.php
@@ -0,0 +1,3 @@
+<a class="btn btn-outline-danger" href="{{ redirect()->back()->getTargetUrl() }}">
+    <span>Cancelar</span>
+</a>
---
+++ resources/views/components/buttons/submit-cancel-btns.blade.php
@@ -0,0 +1,4 @@
+<div class="form-group d-flex justify-content-center">
+    <x-buttons.submit-button />
+    <x-buttons.cancel-button />                            
+</div>
