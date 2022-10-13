b4ca3fad87e8348d255d011c9a74e2889cca6127
Ignacio Dotta <ij.dotta@gmail.com>
2022-10-13 10:36:32 -0300

    create benefit view

---
+++ resources/views/components/forms/input.blade.php
@@ -0,0 +1,10 @@
+<div class="form-group my-2">
+    <div class="row justify-content-center">
+        <div class="col-md-6 col-sm-12">
+            {{$label}}
+        </div>
+        <div class="col-md-6 col-sm-12">
+            {{$input}}
+        </div>
+    </div>
+</div>
