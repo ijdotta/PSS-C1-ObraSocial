30d115b54df2322ecb538d5335e6a49a7ad4c4ef
Tomas Felipe <tomasfelipe911@gmail.com>
2022-10-29 14:30:42 -0300

    Se implemento logica para cupon mensual

--- app/Http/Controllers/PaymentCouponController.php
+++ app/Http/Controllers/PaymentCouponController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\AdultAffiliate;
 use Illuminate\Support\Facades\Auth;
 use PDF;
+use Carbon\Carbon;
 
 class PaymentCouponController extends Controller
 {
@@ -30,9 +31,97 @@ class PaymentCouponController extends Controller
         return view('coupon.generatePaymentCoupon',compact('affiliate'));
     }
 
-    public function request()
+    public function createMonth(Request $request)
     {
-        return view('coupon.requestPaymentCoupon');
+        $request->validate(   
+            ['month' => 'required|integer|between:1,12'] 
+        );
+
+        $requestedMonth=$request->month;
+        $currentMonth=Carbon::now()->month;
+        $currentDay=Carbon::now()->day;
+        $expirationDate=Carbon::now();
+
+        if($currentMonth==$requestedMonth){
+            if($currentDay>=1 && $currentDay<=10){
+                $expirationDate=Carbon::create(Carbon::now()->year, Carbon::now()->month, 10);
+            }
+        }
+
+        if($currentMonth<$requestedMonth){
+            $expirationDate=Carbon::create(Carbon::now()->year, $requestedMonth, 10);  
+        }
+
+        $daysInterest = Carbon::create(Carbon::now()->year, $requestedMonth, 10)->diffInDays(Carbon::now());
+        if($currentMonth<$requestedMonth){
+            $daysInterest = 0;  
+        }
+
+        if($currentMonth==$requestedMonth){
+            if($currentDay>=1 && $currentDay<=10){
+                $daysInterest = 0;
+            }
+        }
+
+        $affiliate=AdultAffiliate::where('user_id','=',Auth::user()->id)->get()->first();
+        $expirationDate=$expirationDate->format('Y-m-d');
+
+        
+        $totalToPay=PaymentCouponController::computeTotalToPay($daysInterest);
+        
+        $payDate=$requestedMonth;
+
+        return view('coupon.generatePaymentCoupon',compact('affiliate','expirationDate','payDate','totalToPay'));
+    }
+
+    public function createSemester(Request $request)
+    {
+ 
+
+        $requestedMonth=$request->month;
+        $currentMonth=Carbon::now()->month;
+        $currentDay=Carbon::now()->day;
+        $expirationDate=Carbon::now();
+
+        if($currentMonth==$requestedMonth){
+            if($currentDay>=1 && $currentDay<=10){
+                $expirationDate=Carbon::create(Carbon::now()->year, Carbon::now()->month, 10);
+            }
+        }
+
+        if($currentMonth<$requestedMonth){
+            $expirationDate=Carbon::create(Carbon::now()->year, $requestedMonth, 10);  
+        }
+
+        $daysInterest = Carbon::create(Carbon::now()->year, $requestedMonth, 10)->diffInDays(Carbon::now());
+        if($currentMonth<$requestedMonth){
+            $daysInterest = 0;  
+        }
+
+        if($currentMonth==$requestedMonth){
+            if($currentDay>=1 && $currentDay<=10){
+                $daysInterest = 0;
+            }
+        }
+
+        $affiliate=AdultAffiliate::where('user_id','=',Auth::user()->id)->get()->first();
+        $expirationDate=$expirationDate->format('Y-m-d');
+
+        
+        $totalToPay=PaymentCouponController::computeTotalToPay($daysInterest);
+        
+        $payDate=$requestedMonth;
+
+        return view('coupon.generatePaymentCoupon',compact('affiliate','expirationDate','payDate','totalToPay'));
+    }
+
+    public function request(Request $request)
+    {
+        $affiliate=AdultAffiliate::where('user_id','=',Auth::user()->id)->get()->first();
+
+        $way_to_pay=$affiliate->way_to_pay;
+
+        return view('coupon.requestPaymentCoupon',compact('way_to_pay'));
     }
 
     public function downloadPDF()
@@ -53,6 +142,33 @@ class PaymentCouponController extends Controller
     public function store(Request $request)
     {
         //
+    }
+
+    function computeTotalToPay($daysInterest)
+    {
+        $affiliate=AdultAffiliate::where('user_id','=',Auth::user()->id)->get()->first();
+    
+        $age=Carbon::parse($affiliate->birthdate)->age;
+
+        if($age<25){
+            $totalToPay=$affiliate->plan->price_under_25;
+        }elseif($age>=25 && $age<40){
+            $totalToPay=$affiliate->plan->price_from_25_to_40;
+        }elseif($age>=40 && $age<60){
+            $totalToPay=$affiliate->plan->price_from_40_to_60;
+        }else{
+            $totalToPay=$affiliate->plan->price_over_60;
+        }
+
+        foreach($affiliate->minorAffiliates as $minor){ 
+            $totalToPay+=$affiliate->plan->price_under_25;
+        }
+        
+        $interest=$daysInterest * ( ($totalToPay * 0.5) /100 ); 
+
+        $totalToPay+=$interest;
+
+        return $totalToPay;
     }
 
     
--- resources/views/coupon/requestPaymentCoupon.blade.php
+++ resources/views/coupon/requestPaymentCoupon.blade.php
@@ -5,7 +5,27 @@
 <div class="pt-5 container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <button type="button" class="btn btn-success" onclick="location.href='{{route('create_payment_coupon')}}'">Obtener cupon de pago</button>
+            @if($way_to_pay=="Anual")
+                <button type="button" class="btn btn-success" onclick="location.href='{{route('create_payment_coupon')}}'">Obtener cupon de pago</button>
+            @elseif($way_to_pay=="Semestral")
+                <button type="button" class="btn btn-success" onclick="location.href='{{route('create_payment_coupon')}}'">Obtener cupon de pago semestral</button>
+            @elseif($way_to_pay=="Mensual")
+                <form method="get" action="{{ route('create_payment_coupon_month') }}" enctype="multipart/form-data">
+                        @csrf
+
+                        <x-errors-alerts />
+
+                        <div class="row align-items-start">
+                            <p class="col">Mes del cupón</p>
+                            <input type="number" class="col form-control" required name="month">
+                        </div>
+
+                        
+                        <div class="post_button">
+                            <button type="submit" class="btn btn-success">Obtener cupon de pago</button>
+                        </div>
+                    </form>
+            @endif
         </div>
     </div>
 </div>
