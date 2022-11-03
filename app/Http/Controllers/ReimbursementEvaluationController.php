657d23f81bd0d27c8b32641a73efa79bfef8627e
Ignacio Dotta <ij.dotta@gmail.com>
2022-11-03 16:54:48 -0300

    fixed ReimbursementEvaluationController namespace problems

--- app/Http/Controllers/ReimbursementEvaluationController.php
+++ app/Http/Controllers/ReimbursementEvaluationController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Enums\BenefitStates;
-use App\Models\reimbursement;
+use App\Models\Reimbursement;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
